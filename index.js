b5da1ac8f74534f916a79756da20b8e62eb814dc
NyxTheMage <nyx.the.mage@gmail.com>
2020-11-12 17:04:38 +1000

    Cleanup and proper cluster handling

--- index.js
+++ index.js
@@ -12,23 +12,27 @@ const elefgy = {
   version: '0.0.0'
 };
 
-const PORT = process.env.PORT || 5000;
-const clusterSize = process.env.WEB_CONCURRENCY || 1;
-const app = express();
-
 // Main process creates workers and listens for their exit
 if (cluster.isMaster) {
-  console.log(`[${elefgy.name}] elefgy.version has started`);
+  console.log(`[${elefgy.name}] ${elefgy.version} has started`);
+  const clusterSize = process.env.WEB_CONCURRENCY || 1;
   for (let i = 0; i < clusterSize; i++) {
     cluster.fork();
   }
-  cluster.on('exit', (worker) => {
-    console.log(`[${elefgy.name} worker ${worker.process.pid} has died`);
+  cluster.on('listening', (worker, address) => {
+    console.log(`[${elefgy.name}] worker ${worker.process.pid}`,
+        `listening on ${address.port}`);
+  });
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`[${elefgy.name}] worker ${worker.process.pid}`,
+        `has exited (${code || signal}) restarting`);
     cluster.fork();
   });
 
 // Each worker creates a server to listen to port
 } else {
+  const PORT = process.env.PORT || 5000;
+  const app = express();
   app.use(helmet());
   app.use(express.static(path.join(__dirname, 'public')));
   app.set('views', path.join(__dirname, 'views'));
@@ -36,7 +40,8 @@ if (cluster.isMaster) {
   app.get('/', (req, res) => {
     res.render('home');
   });
-  app.listen(PORT, () => {
-    console.log(`[${elefgy.name}] worker ${process.pid} listening on ${PORT}`);
+  const server = app.listen(PORT);
+  process.on('exit', () => {
+    server.close();
   });
 }
