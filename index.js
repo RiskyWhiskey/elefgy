7349562cc0a5c3bab10ad68bc5995de143ce9f33
NyxTheMage <nyx.the.mage@gmail.com>
2020-11-12 13:32:08 +1000

    Use templates

--- index.js
+++ index.js
@@ -2,24 +2,28 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-const os = require('os');
 const cluster = require('cluster');
 const express = require('express');
 const path = require('path');
 const helmet = require('helmet');
 
+const elefgy = {
+  name: 'elefgy',
+  version: '0.0.0'
+};
+
 const PORT = process.env.PORT || 5000;
-const clusterSize = os.cpus().length;
+const clusterSize = process.env.WEB_CONCURRENCY || 1;
 const app = express();
 
 // Main process creates workers and listens for their exit
 if (cluster.isMaster) {
-  console.log('elefgy has started');
+  console.log(`[${elefgy.name}] elefgy.version has started`);
   for (let i = 0; i < clusterSize; i++) {
     cluster.fork();
   }
   cluster.on('exit', (worker) => {
-    console.log('Worker', worker.process.pid, 'has exited.');
+    console.log(`[${elefgy.name} worker ${worker.process.pid} has died`);
     cluster.fork();
   });
 
@@ -33,6 +37,6 @@ if (cluster.isMaster) {
     res.render('home');
   });
   app.listen(PORT, () => {
-    console.log('New worker', process.pid, 'listening on', PORT);
+    console.log(`[${elefgy.name}] worker ${process.pid} listening on ${PORT}`);
   });
 }
