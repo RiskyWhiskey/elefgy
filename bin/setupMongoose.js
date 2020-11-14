efe9c290e3e04ec2f1f68a005cb21845d69d6004
NyxTheMage <nyx.the.mage@gmail.com>
2020-11-14 22:51:55 +1000

    Quick fix

--- bin/setupMongoose.js
+++ bin/setupMongoose.js
@@ -1,7 +1,7 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
 
-setupMongoose = {
+const setupMongoose = {
   start: () => {
     mongoose.connect(process.env.DATABASE_URI, {
       useNewUrlParser: true,
